a7558f1b6bfb7631767859abe35d06a7642c4d5d
BackRunner <dev@backrunner.top>
2021-07-09 00:18:05 +0800

    fix: break line in first post

--- src/template/firstPost.ts
+++ src/template/firstPost.ts
@@ -4,6 +4,7 @@ title: 你好，世界
 date: {date}
 ---
 当你看到这篇文章的时候，说明你已经成功初始化了你的Fragy站点。
+
 这篇文章只是一个示例，你可以在 \`.fragy/posts\` 文件夹中删除它。
 `;
 
@@ -13,6 +14,7 @@ title: Hello world
 date: {date}
 ---
 Congratulations, you've created your site successfully with Fragy.
+
 This post is just for demo, you can delete it in \`.fragy/posts\`.
 `;
 

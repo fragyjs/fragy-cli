93e1280894c57e25caddd37abc621b50ef5b99dd
BackRunner <dev@backrunner.top>
2021-07-09 00:54:26 +0800

    feat: simplify first post text

--- src/template/firstPost.ts
+++ src/template/firstPost.ts
@@ -3,9 +3,7 @@ const zhCNTemplate = `
 title: 你好，世界
 date: {date}
 ---
-当你看到这篇文章的时候，说明你已经成功初始化了你的Fragy站点。
-
-这篇文章只是一个示例，你可以在 \`.fragy/posts\` 文件夹中删除它。
+您已经成功初始化了一个全新的Fragy站点，欢迎您使用Fragy。
 `;
 
 const enTemplate = `
@@ -13,9 +11,7 @@ const enTemplate = `
 title: Hello world
 date: {date}
 ---
-Congratulations, you've created your site successfully with Fragy.
-
-This post is just for demo, you can delete it in \`.fragy/posts\`.
+Welcome, you've created your site successfully with Fragy.
 `;
 
 export default {
